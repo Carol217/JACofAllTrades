b4f874a1c65d3e18023b738b9024dc403b1f6786
Carol217 <cpan3@stuy.edu>
2017-04-20 11:48:12 -0400

    added descriptors

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,11 +1,28 @@
 public interface PriorityQueue<T>{
 
+    /****************************
+     * void add(i)
+     * Adds an item to this priority queue.
+     ****************************/
     public void add(T x);
 
+    /****************************
+     * boolean isEmpty()
+     * Returns true if this stack is empty, otherwise returns false.
+     ****************************/ 
     public boolean isEmpty();
 
+    /****************************
+     * Integer peekMin()
+     * Returns the smallest item stored in this priority queue
+     * without removing it.
+     ****************************/
     public T peekMin();
 
+    /****************************
+     * Integer removeMin()
+     * Removes and returns the smallest item stored in this priority queue
+     ****************************/
     public T removeMin();
 
 }
