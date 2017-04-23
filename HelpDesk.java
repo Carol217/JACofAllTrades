878d5ad2b2d3774a1937efb11c9c5c02e19a3a3c
Andrew Qu <aqu3@stuy.edu>
2017-04-23 14:58:52 -0400

    add currentTicket() and solveTicket(), progress made on main()

--- HelpDesk.java
+++ HelpDesk.java
@@ -15,8 +15,8 @@ import cs1.Keyboard;
 public class HelpDesk{
     
     //~~~~~~instance vars~~~~~~~~~~~  
-    private ArrayPriorityQueue _queue;
-    private int counter;
+    private ArrayPriorityQueue _queue; //to hold all these tickets
+    private int counter; //to assign id numbers to each ticket
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     //~~~~~~basic methods~~~~~~~~~~~~
@@ -26,19 +26,29 @@ public class HelpDesk{
     }
 
     public String toString(){
-	return "";
+	return "This HelpDesk has fixed " + counter + " issues!";
     }
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     //~~~~~help desk methods~~~~~~~~~~
+    
     //latest ticket under perusal
     public Ticket currentTicket(){
-	return null;
+	if( _queue.isEmpty() ){
+	    System.out.println("No problems are currently enqueued");
+	    return null;
+	}
+	return _queue.removeMin();
     }
 
     //solve latest problem, and remove ticket from queue
     public boolean solveTicket(){
-	
+	Ticket prob = currentTicket();
+	System.out.println("Now resolving " + prob);
+	System.out.print("What is your solution, IT guy?");
+	String sol = Keyboard.readString();
+	prob.solve(sol);
+	System.out.println("Thank you for your assistance!");
 	return true;
     }
 
@@ -66,6 +76,7 @@ public class HelpDesk{
 	System.out.println("\n\tNew Ticket Created: \n" + tic);
 	System.out.println("\nA professional will fix this problem shortly."
 			   + "\nThank you");
+	_queue.add(tic);
 	counter ++;
 	
     }
@@ -75,7 +86,23 @@ public class HelpDesk{
 
     //main method: user interface time
     public static void main(String[] args){
+	HelpDesk HD = new HelpDesk();
 	//user is given option: solve a problem or request help
+	System.out.println("Welcome to the HelpDesk! Are you here to:");
+	System.out.println("1: Submit an issue");
+	System.out.println("2: Resolve these issues. I work here!");
+	System.out.print("Please select choice 1 or 2: ");
+	int choice = Keyboard.readInt();
+	while(choice != 1 && choice != 2){
+	    System.out.print("Please make a valid selection: ");
+	    choice = Keyboard.readInt();
+	}
+	if(choice == 1)
+	    HD.addTicket();
+	else
+	    HD.solveTicket();
+
+	System.out.println("Thx for the help b");
 	//should go through necessary options to do either
 	//and then maybe a do you still want to work at the help desk?
     }
